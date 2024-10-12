084b1ef6c2e624675de520d40ef4a461ed427d52
temmypearl <temitopepopoola2004@gmail.com>
2024-10-12 11:50:27 +0100

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-document.querySelector('.navbar-toggle').addEventListener
-('click', () => document.querySelector(".nav")
-.classList.toggle('show'));
+// document.querySelector('.navbar-toggle').addEventListener
+// ('click', () => document.querySelector(".nav")
+// .classList.toggle('show'));
+
+function nav
 
 
 function checkEligibility() {
     const age = document.getElementById('age').value;
+    const annualIncome = document.getElementById('annual-income').value;
     const healthCondition = document.getElementById('health-condition').value;
     const resultDiv = document.getElementById('result');
 
     let resultMessage = '';
 
-    if (age >= 65 || healthCondition === 'yes') {
-        resultMessage = 'You qualify for the vaccine!';
+    if (age < 5 && healthCondition === "no" && annualIncome < 4500) {
+        resultMessage = "Eligible for free vaccination.";
     } else {
-        resultMessage = 'You do not qualify for the vaccine at this time.';
+        resultMessage = 'Not eligible for free vaccination.';
     }
 
     resultDiv.textContent = resultMessage;
